0f26890c2a5d1a25b57bcf90652d4f8ea516e584
Chris Bolin <bolin.chris@gmail.com>
2020-11-20 16:53:23 -0700

    build for github pages

---
+++ docs/_next/static/5V3a7DooLTDRTsKUANpuj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ac3fa75c497025e02903.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4173915aae3695671def.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
