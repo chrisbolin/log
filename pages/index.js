7aa5ca4f9c28a2c90751296968152f5e66533fcc
Chris Bolin <bolin.chris@gmail.com>
2020-11-20 17:22:38 -0700

    add head with title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import format from "date-fns/format";
+import Head from "next/head";
 
 import { makeEntries } from "../server/entires";
 
 export default function Index({ entries }) {
   return (
     <div>
+      <Head>
+        <title>Log - Chris Bolin</title>
+      </Head>
       {entries.map(({ html, date }, idx) => (
         <div key={idx}>
           <div>{format(new Date(date), "MMMM d, yyyy")}</div>
