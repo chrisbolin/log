9424688d865a365b751c51f3820fbd9345eb06f4
Chris Bolin <bolin.chris@gmail.com>
2020-11-22 05:47:46 -0700

    remove docs/

--- docs/_next/static/0CP8KkiT6FvZ4clHpFW3p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-710386134abed61ff0b1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-33d04380037f3567cb15.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
