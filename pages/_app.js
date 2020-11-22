5d4bdc40dd7980d3f71bd6c81df74fc3f8b3d08c
Chris Bolin <bolin.chris@gmail.com>
2020-11-22 11:48:38 -0700

    move custom Head to custom App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+import Head from "next/head";
+
 import "./styles.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Log - Chris Bolin</title>
+        <style>
+          {`@font-face {
+            font-family: "Source Code Pro Light";
+            src: url("log/SourceCodePro-Light.ttf");
+          }`}
+        </style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,22 @@
 import format from "date-fns/format";
-import Head from "next/head";
 
 import { makeEntries } from "../server/entires";
 
 export default function Index({ entries }) {
   return (
-    <>
-      <Head>
-        <title>Log - Chris Bolin</title>
-        <style>
-          {`@font-face {
-            font-family: "Source Code Pro Light";
-            src: url("log/SourceCodePro-Light.ttf");
-          }`}
-        </style>
-      </Head>
-      <main>
-        {entries.map(({ html, date }, idx) => (
-          <div className="entry" key={idx}>
-            <div className="date">
-              {format(new Date(date), "MMM dd yyyy").toUpperCase()}
-            </div>
-            <div className="text">
-              <div dangerouslySetInnerHTML={{ __html: html }} />
-              <hr />
-            </div>
+    <main>
+      {entries.map(({ html, date }, idx) => (
+        <div className="entry" key={idx}>
+          <div className="date">
+            {format(new Date(date), "MMM dd yyyy").toUpperCase()}
           </div>
-        ))}
-      </main>
-    </>
+          <div className="text">
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <hr />
+          </div>
+        </div>
+      ))}
+    </main>
   );
 }
 
